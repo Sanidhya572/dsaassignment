9c11ea5446e12aa211f166155ab0079c507355bb
Sanidhya572 <84410647+Sanidhya572@users.noreply.github.com>
2021-09-03 23:49:50 +0530

    Add files via upload

--- dsaassigment.cpp
+++ dsaassigment.cpp
@@ -62,3 +62,48 @@ int main()
         }
         return 0;
 }
+
+
+Q3.
+//
+#include <iostream>
+#include <conio.h>
+#define SIZE 2;
+
+void main ()
+{
+  int arr[SIZE][SIZE][SIZE];
+  int i, j, k;
+  clrscr ();
+  for (i = 0; i < SIZE; i++)
+    {
+      for (j = 0; j < SIZE; j++)
+	{
+	  for (k = 0; k < SIZE; k++)
+	    arr[i][j][k] = i + j + k;
+	}
+    }
+  cout << "\n Elements in Row major order are: \n";
+  for (i = 0; i < SIZE; i++)
+    {
+      for (j = 0; j < SIZE; j++)
+	{
+	  for (k = 0; k < SIZE; k++)
+	    arr[i][j][k] = i + j + k;
+	}
+    }
+  cout << "\nElements in colum major order\n";
+  for (i = 0; i < SIZE; i++)
+    {
+      for (j = 0; j < SIZE; j++)
+	{
+	  for (k = 0; k < SIZE; k++)
+	    cout << "[" << i << "]" << "[" << j << "]" << "[" << k << "]" <<
+	      "=" << arr[i][j][k];
+	  cout << "\n\n";
+	}
+    }
+
+getch();
+
+
