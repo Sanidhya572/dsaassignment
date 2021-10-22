8acf9c799a0cc5c96c683817f8e949b95921d610
Sanidhya572 <84410647+Sanidhya572@users.noreply.github.com>
2021-10-22 09:50:22 +0530

    Update linkedlist_insertionatfirst.cpp

--- linkedlist_insertionatfirst.cpp
+++ linkedlist_insertionatfirst.cpp
@@ -35,14 +35,18 @@ first->next=second;
 
 second->data=20;
 second->next=third;
+
 third->data=30;
 third->next=fourth;
+
 fourth->data=40;
 fourth->next=fifth;
+
 fifth->data=50;
 fifth->next=NULL;
+
 cout<<"FIRST==>";
-//to print the elements of linked list
+//for print the elements of linked list
 printlinkedlist(first);
 first=insert(first,5);
 cout<<"after inserting an element at the first position"<<endl;
